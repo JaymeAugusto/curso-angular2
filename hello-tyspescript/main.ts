7d66fc201cc56091c2e47e705eec062a4865757f
Jayme Augusto <ja.guto01@gmail.com>
2021-07-05 19:28:29 -0300

    Módulos (ngModule)

--- hello-tyspescript/main.ts
+++ hello-tyspescript/main.ts
@@ -1,11 +1,11 @@
 var minhaVar = 'minha variavel';
 
-function minhaFunc(x,y){
-    return x + y;
-}
+// function minhaFunc(x,y){
+//     return x + y;
+//}
 
-let num = 2;
-const PI = 3.14;
+// let num = 2;
+// const PI = 3.14;
 
 var numeros = [1,2,3];
 
@@ -16,11 +16,11 @@ numeros.map(function(valor){
 numeros.map(valor => valor*2);
 
 
-class Matematica {
-    soma(x, y){
-        return x + y;
-    }
-}
+// class Matematica {
+//     soma(x, y){
+//         return x + y;
+//     }
+// }
 
 var n1 = 'addssd';
-n1 = 4;
+//n1 = 4;
