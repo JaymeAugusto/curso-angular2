0a32f261a61044f307430a44b957070a21c826e7
Jayme Augusto <ja.guto01@gmail.com>
2021-07-11 18:37:49 -0300

    Diretivas: ngFor

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
 import { DiretivaNgSwitchComponent } from './diretiva-ng-switch/diretiva-ng-switch.component';
+import { DiretivaNgForComponent } from './diretiva-ng-for/diretiva-ng-for.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DiretivaNgifComponent,
-    DiretivaNgSwitchComponent
+    DiretivaNgSwitchComponent,
+    DiretivaNgForComponent
   ],
   imports: [
     BrowserModule,
