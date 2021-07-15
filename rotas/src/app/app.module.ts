8968bf2d15be8eba26798573f3c21015bbdc69e8
Jayme Augusto <ja.guto01@gmail.com>
2021-07-15 20:58:58 -0300

    Criando um módulo de funcionalidade

--- rotas/src/app/app.module.ts
+++ rotas/src/app/app.module.ts
@@ -1,31 +1,35 @@
-import { CursosService } from './cursos/cursos.service';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CursosComponent } from './cursos/cursos.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { routing } from './app.routing';
-import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
-import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
+import { CursosModule } from './cursos/cursos.module';
+// import { CursosComponent } from './cursos/cursos.component';
+// import { CursosService } from './cursos/cursos.service';
+// import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
+// import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CursosComponent,
     LoginComponent,
     HomeComponent,
-    CursoDetalheComponent,
-    CursoNaoEncontradoComponent
+    // CursosComponent,
+    // CursoDetalheComponent,
+    // CursoNaoEncontradoComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule,
+    CursosModule,
     routing
   ],
-  providers: [CursosService],
+  // providers: [CursosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
