7b104c50528a8e9f6bb3132f6cdcef2323d47cca
Jayme Augusto <ja.guto01@gmail.com>
2021-07-19 19:03:58 -0300

    Inicializando valores com ngModel (two-way data-binding)

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -7,8 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateFormComponent implements OnInit {
 
+  usuario : any = {
+    nome: 'jayme',
+    email: 'jayme@email.com'
+  }
+
   onSubmit(form: any){
     console.log(form);
+
+    console.log(this.usuario);
+    
   }
 
   constructor() { }
