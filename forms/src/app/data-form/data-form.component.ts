6aa915c6b68ae8f8dfea51ab58e422244a0e24c8
Jayme Augusto <ja.guto01@gmail.com>
2021-08-08 17:15:58 -0300

    Validação Customizada (CEP)

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -48,7 +48,7 @@ export class DataFormComponent implements OnInit {
       email: [null, [Validators.required, Validators.email]],
       
       endereco: this.formBuilder.group({
-        cep: [null, [Validators.required]],
+        cep: [null, [Validators.required, FormValidations.cepValidator]],
         numero: [null, Validators.required],
         complemento: [null],
         rua: [null, Validators.required],
@@ -127,7 +127,13 @@ export class DataFormComponent implements OnInit {
   }
 
   verificaValidTouched(campo: string){
-    return !this.formulario.get(campo)?.valid && (!!this.formulario.get(campo)?.touched || !!this.formulario.get(campo)?.dirty);
+    return !this.formulario.get(campo)?.valid && 
+    (!!this.formulario.get(campo)?.touched || !!this.formulario.get(campo)?.dirty);
+  }
+
+  verificaRequired(campo: string){
+    return !!this.formulario.get(campo)?.hasError('required') && 
+    (!!this.formulario.get(campo)?.touched || !!this.formulario.get(campo)?.dirty);
   }
 
   verificaEmailInvalido(){
